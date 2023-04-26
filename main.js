41a3ffc66ea4bed26320ec94392551519aa9e602
korban-mantan <mubarokakbar13@gmail.com>
2023-04-26 16:34:55 +0700

    added canvas and animation

--- main.js
+++ main.js
@@ -48,3 +48,54 @@ muteButton.addEventListener('click', () => {
   // save muted state to local storage
   localStorage.setItem('isMuted', audioElement.muted)
 })
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const image = new Image()
+image.src = './assets/img/jupiter.png'
+
+image.onload = () => {
+  // const canvasWidth = canvas.width
+  // const canvasHeight = canvas.height
+  // const imageWidth = image.width / 2
+  // const imageHeight = image.height / 2
+
+  // // Calculate the position where we want to draw the image
+  // const x = canvasWidth / 2 - imageWidth / 2
+  // const y = canvasHeight / 2 - imageHeight / 2
+
+  // // Draw the image on the canvas
+  // ctx.drawImage(image, x, y, imageWidth, imageHeight)
+
+  let angle = 0
+
+  // Define the animation loop
+  function animate () {
+    // Clear the canvas before drawing the image
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    // Save the current context state
+    ctx.save()
+
+    // Translate the origin to the center of the canvas
+    ctx.translate(canvas.width / 2, canvas.height / 2)
+
+    // Rotate the canvas around the center
+    ctx.rotate(angle * Math.PI / 180)
+
+    // Draw the image at the center of the canvas
+    ctx.drawImage(image, -image.width / 8, -image.height / 8, image.width / 4, image.height / 4)
+
+    // Restore the context state
+    ctx.restore()
+
+    // Increase the angle of rotation for the next frame
+    angle += 1
+
+    // Request the next animation frame
+    requestAnimationFrame(animate)
+  }
+
+  // Start the animation loop
+  animate()
+}
